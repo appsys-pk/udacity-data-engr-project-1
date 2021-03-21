64856ba459e8ac0b31481134f5bd2650b0ec8f54
shakirfattani <m.shakirfattani@gmail.com>
2021-03-21 22:32:21 +0500

    etl.ipynb completed

--- sql_queries.py
+++ sql_queries.py
@@ -4,12 +4,12 @@ songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS times"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
-id SERIAL,
+id SERIAL PRIMARY KEY,
 start_time TIMESTAMP,
 user_id INT,
 level VARCHAR,
@@ -21,7 +21,7 @@ user_agent VARCHAR
 )""")
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
-user_id INT, 
+user_id INT PRIMARY KEY, 
 first_name VARCHAR,
 last_name VARCHAR, 
 gender VARCHAR, 
@@ -29,7 +29,7 @@ level VARCHAR
 )""")
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
-song_id VARCHAR, 
+song_id VARCHAR PRIMARY KEY, 
 title VARCHAR,
 artist_id VARCHAR, 
 year INT, 
@@ -37,7 +37,7 @@ duration NUMERIC
 )""")
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
-artist_id VARCHAR, 
+artist_id VARCHAR PRIMARY KEY, 
 name VARCHAR,
 location VARCHAR, 
 lattitude NUMERIC, 
@@ -45,7 +45,7 @@ longitude NUMERIC
 )""")
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
-start_time TIMESTAMP, 
+start_time TIMESTAMP PRIMARY KEY, 
 hour INT, 
 day INT,
 week INT, 
@@ -64,28 +64,36 @@ VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, lattitude, longitude)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
 
-song_select = ("""
-""")
+song_select = ("""SELECT a.song_id, b.artist_id
+FROM songs a
+    JOIN artists b ON (b.artist_id = a.artist_id)
+WHERE   a.title = %s
+    AND     a.artist_id = %s
+    AND     a.duration = %s""")
 
 # QUERY LISTS
 
